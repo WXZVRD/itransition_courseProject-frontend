c346a5b40b3b292cfdd8ff292579ccb0b57fd9c7
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2023-09-21 15:19:42 +0600

    Update authButton.tsx

--- src/components/authButton.tsx
+++ src/components/authButton.tsx
@@ -11,7 +11,7 @@ interface IAuthProps {
 const AuthButton: FC<IAuthProps> = ({socialIcon, socialTitle }) => {
 
     return(
-            <Link to={`https://itransition-courseproject-backend.onrender.com/${socialTitle}`}>
+            <Link to={`https://itransition-courseproject-backend.onrender.com/auth/${socialTitle}`}>
                 <Button
                     component="button"
                     size={"small"}
