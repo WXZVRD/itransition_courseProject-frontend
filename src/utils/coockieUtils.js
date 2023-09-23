29439a68d13427de0ec6a71a293c294e3f5b8893
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2023-09-23 13:44:35 +0600

    Update coockieUtils.js

--- src/utils/coockieUtils.js
+++ src/utils/coockieUtils.js
@@ -1,39 +1,16 @@
+import Cookies from 'js-cookie';
+
 export function getTokenFromCookie() {
-    console.log("Utils token start...")
-    const cookies = document.cookie.split('; ');
-    console.log("Cookie is ready!")
-    console.log(cookies)
-    for (const cookie of cookies) {
-        const [name, value] = cookie.split('=');
-        if (name === 'jwt') {
-            console.log("Whe found JWT")
-            return value;
-        }
-    }
-    console.log("Nothing was founded")
-    return null;
+    const token = Cookies.get('jwt');
+    return token || null;
 }
 
 export function getUserDataFromCookie() {
-    const cookies = document.cookie.split('; ');
-    for (const cookie of cookies) {
-        const [name, value] = cookie.split('=');
-        if (name === 'user') {
-            const decodedValue = decodeURIComponent(value);
-            try {
-                return JSON.parse(decodedValue);
-            } catch (error) {
-                console.error('Error parsing user data:', error);
-            }
-        }
-    }
-    return null;
+    const userData = Cookies.get('user');
+    return userData || null;
 }
 
 export function clearCookies() {
-    const cookies = document.cookie.split('; ');
-    for (const cookie of cookies) {
-        const [name, _] = cookie.split('=');
-        document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    }
+    Cookies.remove('jwt');
+    Cookies.remove('user');
 }
