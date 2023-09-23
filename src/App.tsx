f136b89844eb149690c63818381db9a9f596c2d5
WXZVRD <134217021+WXZVRD@users.noreply.github.com>
2023-09-23 13:44:04 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import ReviewMaker from './pages/ReviewMaker';
 import Profile from './pages/Profile';
@@ -8,41 +8,43 @@ import Admin from './pages/Admin';
 import Home from './pages/Home';
 
 import { ThemeProvider } from '@mui/material/styles';
-import {darkTheme, lightTheme} from './Themes'
+import { darkTheme, lightTheme } from './Themes';
 
-import {getTokenFromCookie, getUserDataFromCookie} from "./utils/coockieUtils";
-import {getAuthData} from "./redux/slices/authSlice";
-import {useAppDispatch, useAppSelector} from "./redux/hooks";
+import { getTokenFromCookie, getUserDataFromCookie } from './utils/coockieUtils';
+import { getAuthData } from './redux/slices/authSlice';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
 
 function App() {
-    const dispatch = useAppDispatch()
-    const theme = useAppSelector(state => state.app.theme)
+    const dispatch = useAppDispatch();
+    const theme = useAppSelector(state => state.app.theme);
 
     useEffect(() => {
         console.log("Getting token...")
-        const token = getTokenFromCookie()
-        if (token){
+        const token = getTokenFromCookie();
+        if (token) {
             console.log("We have token...")
-            const user = getUserDataFromCookie()
-            dispatch(getAuthData(user))
+            const user = getUserDataFromCookie();
+            if (user) {
+                dispatch(getAuthData(JSON.parse(user)));
+            }
         }
         console.log("Ending useEffect...")
-    }, [])
+    }, [dispatch]);
 
     return (
         <div className="App">
-               <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
-                   <BrowserRouter>
-                           <Routes>
-                               <Route path="/post/edit/:id" element={<ReviewMaker />}/>
-                               <Route path="/post/create" element={<ReviewMaker/>}/>
-                               <Route path="/profile/:id" element={<Profile/>}/>
-                               <Route path="/post/:id" element={<Review/>}/>
-                               <Route path="/admin" element={<Admin/>}/>
-                               <Route path="/" element={<Home />} />
-                           </Routes>
-                   </BrowserRouter>
-               </ThemeProvider>
+            <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/post/edit/:id" element={<ReviewMaker />} />
+                        <Route path="/post/create" element={<ReviewMaker />} />
+                        <Route path="/profile/:id" element={<Profile />} />
+                        <Route path="/post/:id" element={<Review />} />
+                        <Route path="/admin" element={<Admin />} />
+                        <Route path="/" element={<Home />} />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
         </div>
     );
 }
